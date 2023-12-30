87af9f6778bdb83e67fa835ff896ad9c3eff1adc
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2023-12-30 13:21:22 -0800

    skeleton of travel app

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -4,32 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const initialItems = [{id:1,description:"Passports",quanity:2,packed:false},{id:2,description:"PJs",quanity:12,packed:false}]
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='app'>
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+       </div>
     </>
   )
 }
+function Logo(){
+  return <h1>Far away</h1>
+}
+function Form(){
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  )
+}
+function PackingList(){
+  return <div className="list">List</div>
+}
+function Stats(){
+  return (
+    <footer className="stats">
+      <em>Your have x items packed in your bag and ypu have x to go.</em>
+    </footer>
+  )
+}
+
+
 
 export default App
