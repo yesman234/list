d394ebf0f9536873f6506674e83906a1cc594ab2
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2024-01-04 11:01:32 -0800

    onChange w/ handleSubmit showing in console

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState} from 'react'
 import './App.css'
-const initialItems = [{id:1,description:"Passports",quanity:5,packed:false},{id:2,description:"PJs",quanity:12,packed:false}]
+const initialItems = [{id:1,description:"Passports",quanity:5,packed:false},{id:4,description:"Currency",quanity:7,packed:true},{id:2,description:"PJs",quanity:12,packed:false}]
 
 function App() {
   return (
@@ -20,16 +18,29 @@ function Logo(){
   return <h1>Far away</h1>
 }
 function Form(){
+  const [description,setDescription] = useState('');
+  const [quanity, setQuanity] = useState(5);
+  function handleSubmit(e){
+     e.preventDefault();
+     const newItem = {description,quanity,packed:false,id:Date.now()}
+     console.log(newItem)
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    <select>
+    <select value={quanity} onChange={(e)=>setQuanity(Number(e.target.value))}>
      {Array.from({length: 20},(_,i)=>i + 1).map((num=>
      <option value={num} key={num}>
       {num}
       </option>
       ))}
     </select>
+    <input type="text" placeholder='...Item' value={description}
+    onChange={(e)=>{
+      setDescription(e.target.value);
+    }}  
+    />
+
     <button>Add</button>
     </form>
   )
